f08b92c173e24c013e5b8a1a22f4ec985c096d50
raj-savsani <rrsavsani@gmail.com>
2021-12-24 20:47:33 +0530

    assigment done

---
+++ unit5/React/hooks/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter initial={0}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ unit5/React/hooks/src/components/Counter.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import("./Counter.css");
+
+function Counter({ initial }) {
+  const [count, setCount] = useState(initial);
+
+  const chnageCount = (value) => {
+    if (value === "+") {
+      return setCount(count + 1);
+    } else if (value === "-") {
+      return setCount(count - 1);
+    } else if (value === "*") {
+      return setCount(count * 2);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Counter</h1>
+      <h1>{count}</h1>
+      <button onClick={() => chnageCount("+")}>Add 1</button>
+      <button onClick={() => chnageCount("-")}>Reduce 1</button>
+      <button onClick={() => chnageCount("*")}>Double</button>
+    </div>
+  );
+}
+
+export default Counter;
