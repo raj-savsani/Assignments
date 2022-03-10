e4d8d97376fa066a2fd92463b13860a3ba2d47e8
raj-savsani <rrsavsani@gmail.com>
2022-03-10 16:17:11 +0530

    infinitescroll implimented

--- unit5/React/antd/src/components/InfiniteScroll.jsx
+++ unit5/React/antd/src/components/InfiniteScroll.jsx
@@ -1,23 +1,51 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 function InfiniteScroll() {
+  const [page, setPage] = useState(1);
 
-   const [page, setPage] = useState(1);
+  const [photos, setPhotos] = useState([]);
 
-   const [photos, setPhotos] = useState([]);
 
   useEffect(() => {
     getData();
-  }, []);
+
+    return () => {};
+  }, [page]);
 
   const getData = async () => {
-    let {data} = await axios.get(`https://jsonplaceholder.typicode.com/photos?_page=${page}`);
-    setPhotos([...photos,...data]);
-};
-console.log(photos);
+    try {
+      let { data } = await axios.get(
+        `https://jsonplaceholder.typicode.com/photos?_page=${page}`
+      );
+      setPhotos([...photos, ...data]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  // console.log(photos);
 
-  return <div>InfiniteScroll</div>;
+  window.onscroll = function () {
+    // console.log(window.innerHeight + window.scrollY,document.documentElement.scrollHeight)
+    if (
+      window.innerHeight + window.scrollY ===
+      document.documentElement.scrollHeight
+    ) {
+
+      setPage(()=> page + 1)
+
+    }
+  };
+
+  return <div>
+    {
+      photos.length > 0 && photos.map((photo)=> {
+        return <div key={photo.id}> 
+        <img src={photo.url} alt={photo.url}/>
+        </div>
+      })
+    }
+  </div>;
 }
 
 export default InfiniteScroll;
