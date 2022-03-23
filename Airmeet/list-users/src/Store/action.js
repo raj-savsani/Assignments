7cc05a2756d9325ca0ed0cb7c9753481f391242f
raj-savsani <rrsavsani@gmail.com>
2022-03-23 23:30:54 +0530

    created redux store for state management

---
+++ Airmeet/list-users/src/Store/action.js
@@ -0,0 +1,24 @@
+import {
+  GET_USER_ERROR,
+  GET_USER_LOADING,
+  GET_USER_SUCCESS,
+} from "./actionTypes";
+
+export const getUserLoading = () => ({
+  type: GET_USER_LOADING,
+});
+
+export const getUserSuccess = (data) => ({
+  type: GET_USER_SUCCESS,
+  payload: data,
+});
+
+export const getUserError = (err) => ({
+  type: GET_USER_ERROR,
+  payload: err,
+});
+
+//   export const setUser = (data) => ({
+//     type: SET_USER,
+//     payload: data,
+//   });
---
+++ Airmeet/list-users/src/Store/reducer.js
@@ -0,0 +1,40 @@
+import {
+    GET_USER_ERROR,
+    GET_USER_LOADING,
+    GET_USER_SUCCESS,
+    
+  } from "./actionTypes";
+  
+  const init = { loading: true, users: [], error: false };
+  
+  export const reducer = (state = init, { type, payload }) => {
+    switch (type) {
+      case GET_USER_LOADING:
+        return {
+          ...state,
+          loading: true,
+        };
+  
+      case GET_USER_SUCCESS:
+        return {
+          ...state,
+          users: payload,
+          loading: false,
+        };
+  
+      case GET_USER_ERROR:
+        return {
+          ...state,
+          loading: false,
+          error: true,
+        };
+  
+    //   case SET_USER:
+    //     return {
+    //       ...state,
+    //       users: payload,
+    //     };
+      default:
+        return state;
+    }
+  };
